19a1c6aa5efb8f04619de50c2dd20ee0b36a77b2
bzorbozan <beliz.zorbozan@gmail.com>
2023-01-21 18:23:14 -0500

    Removes  whitespace and gives event descrip list

--- webToText.py
+++ webToText.py
@@ -1,12 +1,4 @@
-# I will be using this as a trial document for multiple things. 
-
-# Just so I dont go insane
-    # 1) try extract text
-    # 2) try api
-    # 3) do it manual like sahil 
-
-
-# STEP 1: Try to see if you could extract text from the document. 
+# This program is to extract text from a webpage. 
 
 import datetime
 import requests
@@ -23,13 +15,10 @@ r = requests.get('https://www.eventbrite.ca/d/canada--toronto/events/', headers=
 soup = BeautifulSoup(r.content, 'lxml') #r.content is the actual html_doc
 
 eventLinks = [] # links for each event's page. 
-tabContent = soup.find_all('div', class_="eds-event-card-content__primary-content")
+tabContent = soup.find_all('div', class_="eds-event-card-content__primary-content") #locate the "tab" of events
 for item in tabContent:
-    for link in item.find_all('a', href=True):
+    for link in item.find_all('a', href=True): #where you get the link
         eventLinks.append(link['href'])
-
-#print(eventLinks)
-#testlink = 'https://www.eventbrite.com/e/witches-night-in-the-ultimate-fairytale-mystic-expo-50-tattoos-tickets-468593595057?aff=ebdssbcitybrowse'  #used in the place of event_link
 
 eventTexts = [] # this is the list that will store the descriptions for our event. (Andrew's ML will later on filter the correct info from this.)
 
@@ -37,6 +26,15 @@ for event_link in eventLinks:
     r_event = requests.get(event_link, headers= headers)
     event_html_doc = r_event.content
     soup_event = BeautifulSoup(event_html_doc, 'html.parser')
-    event_text = soup_event.get_text()
-    eventTexts.append(event_text.strip())
+    event_text1 = soup_event.get_text().strip() #this has the weird whitespace in the middle for eventbrite
 
+    description_heading = (str(soup_event.title).replace("<title>","").replace("</title>",""))
+    event_text2 = event_text1.replace(description_heading, "").strip() #only the description left. (i presume that if there is no whitespace issue then this wont change much)
+    full_event_text = description_heading, event_text2
+
+    eventTexts.append(full_event_text)
+
+#for testing:
+# for description in eventTexts:
+#     print(description)
+# print(len(eventTexts))
